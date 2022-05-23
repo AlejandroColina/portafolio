ec9f929ff81f775ef5e4fc99b1a24768e955a8c9
AlejandroColina <alejo_9102@hotmail.com>
2022-05-23 08:25:55 -0500

    clint

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
+import React, { useEffect } from 'react'
 
 function App() {
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log(position)
+    });
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>AQUI ESTOY</h1>
-        
+
       </header>
     </div>
   );
---
+++ client/src/componentes/AboutMe/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function About
+() {
+  return (
+    <h1>ABOUT ME</h1>
+  )
+}
+
+export default About
