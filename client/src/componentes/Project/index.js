895334129849434f44160bd7ff642ac2e708889b
AlejandroColina <alejo_9102@hotmail.com>
2022-06-13 22:18:24 -0500

    varios

--- client/src/componentes/Project/index.js
+++ client/src/componentes/Project/index.js
@@ -4,7 +4,17 @@ import styles from './styles.module.css';
 function Projects() {
     return (
         <section className={styles.principal} id='Projects'>
-            <h1>Projects</h1>
+            <div className={styles.divProject}>
+                <div className={styles.video} ></div>
+                <div className={styles.info} >
+                    <h2>Nombre Projecto</h2>
+                    <p>
+                        Aquí va la descripción del proyecto.
+                        Todo lo necesario para dejar claro lo que
+                        se hizo. De qué manera lo llevé a cabo.
+                    </p>
+                </div>
+            </div>
         </section>
     )
 }
